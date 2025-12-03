968bf4fa6b598ecefd5df1916a6e7bc335599f37
Robert Burger <robert.burger@dlr.de>
2025-12-03 11:00:41 +0100

    change: switched to serialcan stable package

--- conanfile.py
+++ conanfile.py
@@ -6,8 +6,8 @@ class MainProject(ConanFile):
 
     name = "module_slcan"
     description = ""
-    exports_sources = ["*", "!.gitignore"] 
-    
+    exports_sources = ["*", "!.gitignore"]
+
     def source(self):
         self.run(f"sed 's/AC_INIT(.*/AC_INIT([module_slcan], [{self.version}], [{self.author}])/' configure.ac.in > configure.ac")
 
@@ -16,5 +16,5 @@ class MainProject(ConanFile):
         self.requires("service_provider_memory_inspection/[~6]@robotkernel/unstable")
         self.requires("service_provider_canopen_protocol/[~6]@robotkernel/unstable")
         self.requires("service_provider_process_data_inspection/[~6]@robotkernel/unstable")
-        self.requires("serialcan/0.3.0@3rdparty/unstable")
+        self.requires("serialcan/[*]@3rdparty/stable")
 
