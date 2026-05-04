fed4e4a481ced1e3c6158c17129eb234fb8e265e
Robert Burger <robert.burger@dlr.de>
2026-05-04 06:52:38 +0200

    conan_template v6

--- conanfile.py
+++ conanfile.py
@@ -1,15 +1,12 @@
 from conan import ConanFile
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/[~5]@robotkernel/stable"
+    python_requires = "conan_template/[~6]@robotkernel/unstable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "module_slcan"
     description = ""
     exports_sources = ["*", "!.gitignore"]
-
-    def source(self):
-        self.run(f"sed 's/AC_INIT(.*/AC_INIT([module_slcan], [{self.version}], [{self.author}])/' configure.ac.in > configure.ac")
 
     def requirements(self):
         self.requires("robotkernel/[~6]@robotkernel/unstable")
